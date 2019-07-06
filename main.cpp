1af8e975da1cbfc86e180d2b94b51db1b27aed8b
lnsf <lnsf03@gmail.com>
2019-07-06 17:40:46 +0900

    :+1: Executable

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "table.h"
+using namespace std;
+using namespace tbl;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    Table t = Table(3, 3);
+    t.add_row(vector<string>{"hello", "world", "everyone"});
+    t.add_row(vector<string>{"hello", "world", "everyone"});
+    t.add_row(vector<string>{"hello", "world", "everyone"});
+
+    t.show();
+
     return 0;
 }
