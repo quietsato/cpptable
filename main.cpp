91a06bf06718539dfc85048760ee1e9d8314eedb
lnsf <lnsf03@gmail.com>
2019-07-07 10:41:20 +0900

    :+1: Add runner

--- main.cpp
+++ main.cpp
@@ -4,13 +4,46 @@ using namespace std;
 using namespace tbl;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto t = Table(10, 10);
 
-    Table t = Table(3, 3);
-    t.add_row(vector<string>{"hello", "world", "everyone"});
-    t.add_row(vector<string>{"hello", "world", "everyone"});
-    t.add_row(vector<string>{"hello", "world", "everyone"});
+    vector<string> line;
+    int mul;
+    for(int i = 0; i <= 9; i++){
+      for(int j = 0; j <= 9; j++){
+        if(i == 0 && j == 0){
+          line.push_back("  ");
+        }
+        else if(i == 0 || j == 0){
+          line.push_back(to_string(max(i, j)).insert(0, " "));
+        }
+        else{
+          mul = i * j;
+          if(mul < 10)
+            line.push_back(to_string(mul).insert(0, " "));
+          else
+            line.push_back(to_string(mul));
+        }
+      }
+      t.add_row(line);
+      line.clear();
+    }
 
+    t.show();
+
+    t.setStyle(ALL);
+
+    t.show();
+
+    t.setStyle(COL_TITLE);
+
+    t.show();
+
+    t.setStyle(ROW_TITLE);
+
+    t.show();
+
+    t = Table(1, 1);
+    t.add_row(vector<string>{"Hello"});
     t.show();
 
     return 0;
